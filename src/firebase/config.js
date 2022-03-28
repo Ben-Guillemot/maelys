9f09a0e7167e41257d0ba8a2350f7810a84742c7
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-03-28 11:52:05 +0200

    add firebase & created firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA91oXkIFRgGZwx5MuQgKDlnWWZgZjP8-0',
+  authDomain: 'maelys-190cc.firebaseapp.com',
+  projectId: 'maelys-190cc',
+  storageBucket: 'maelys-190cc.appspot.com',
+  messagingSenderId: '762057722630',
+  appId: '1:762057722630:web:4946584dec1b913ad76c87',
+  measurementId: 'G-HXVXLFEYJ7',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
