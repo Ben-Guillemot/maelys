8505bddccc5fda2be67b904ecd47b2ff321a7855
Ben-Guillemot <ben.guillemot1@gmail.com>
2022-05-02 09:42:57 +0200

    add shadow on homepage photos

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import EventFacebook from '../EventFacebook/EventFacebook';
-import photo1 from '../../Assets/photo-homepage-1.png';
-import photo2 from '../../Assets/photo-homepage-2.png';
-import photo3 from '../../Assets/photo-homepage-3.png';
+import photo1 from '../../Assets/photo-homepage-1.webp';
+import photo2 from '../../Assets/photo-homepage-2.webp';
+import photo3 from '../../Assets/photo-homepage-3.webp';
 
 import './homepage.scss';
 
